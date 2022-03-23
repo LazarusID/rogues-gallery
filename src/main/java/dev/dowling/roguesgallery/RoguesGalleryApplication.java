53287fd14e37797e03cb5debc7bd2ba195570d84
Clay Dowling <clay@lazarusid.com>
2022-03-23 16:57:57 -0400

    Initial import

---
+++ src/main/java/dev/dowling/roguesgallery/RoguesGalleryApplication.java
@@ -0,0 +1,13 @@
+package dev.dowling.roguesgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoguesGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoguesGalleryApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V1_0_initial.sql
@@ -0,0 +1,22 @@
+
+create table user (
+    id serial primary key,
+    email varchar(60) not null unique,
+    name varchar(60) not null default 'User'
+);
+
+create table campaign (
+    id serial primary key,
+    user_id integer not null REFERENCES user (id),
+    name varchar(60) not null,
+    active boolean not null default true
+);
+
+create table gallery (
+    id serial primary key,
+    campaign_id integer not null REFERENCES campaign (id),
+    name varchar(60) not null,
+    content text not null default '',
+    cache text default '',
+    active boolean not null default true
+);
